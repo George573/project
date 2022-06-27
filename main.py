4355016b9400429fe65fe7351e65281776ac89d4
George573 <zhora.hhhh@gmail.com>
2022-06-27 11:45:17 +0300

    second commit

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+from operator import pos
 import time
 
 req = open("queries.txt", "r")
 strings = req.readlines()
 
-i = 0
+position = open("position.txt", "r")
+i = int(position.read().strip())
+position.close()
+position = open("position.txt", "w")
+
 while 1:
     print(strings[i%100])
     i += 1
+    position.write(str(i))
     time.sleep(1)
-    
 
 req.close()
+position.close()

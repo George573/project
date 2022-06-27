a94e2e45bcbe1f689b30d1b3e7cb67717deedc1b
George573 <zhora.hhhh@gmail.com>
2022-06-27 13:00:43 +0300

    6th commit

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ import time
 with open("queries.txt") as req:
     strings = req.readlines()
 
-    for i in range (0, len(strings)):
-        strings[i] = strings[i].replace("\n", '')
+for i in range (0, len(strings)):
+    strings[i] = strings[i].replace("\n", '')
 
 
-    with open("position.txt") as position:
-        i = int(position.read().strip())
+with open("position.txt") as position:
+    i = int(position.read().strip())
 
-    with open("position.txt", "w") as position:
-        while 1:
-            print(strings[i % 100])
-            i += 1
-            position.write(str(i))
-            time.sleep(1)
+with open("position.txt", "w") as position:
+    while 1:
+        print(strings[i % 100])
+        i += 1
+        position.write(str(i))
+        time.sleep(1)

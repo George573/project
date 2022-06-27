c15f95f55d39062aaac832156bde6851dc92ecee
George573 <zhora.hhhh@gmail.com>
2022-06-27 14:12:56 +0300

    7th commit

--- main.py
+++ main.py
@@ -3,17 +3,25 @@ from ctypes import sizeof
 import time
 
 with open("queries.txt") as req:
-    strings = req.readlines()
+    strings = [line.rstrip() for line in req]
 
+clean_strings = []
 for i in range (0, len(strings)):
-    strings[i] = strings[i].replace("\n", '')
+    if len(strings[i]) != 0:
+        clean_strings.append(strings[i])
 
+"""leng = len(strings)
+for i in range (0, leng):
+    if len(strings[i]) == 0:
+        strings.pop(i)
+        leng -= 1
+        i -= 1"""
 
 with open("position.txt") as position:
     i = int(position.read().strip())
 
 while 1:
-    print(strings[i % 100])
+    print(clean_strings[i % 100])
     i += 1
     with open("position.txt", "w") as position:
         position.write(str(i))

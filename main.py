5243aeb8321e570e0ecc87d552cf502424730d5e
CharlotteUwU <charlotteuwu@192.168.1.75>
2022-07-02 22:59:50 +0300

    10th commit

--- main.py
+++ main.py
@@ -1,28 +1,52 @@
-import sys
-import time
+import sys, time, threading, os, signal
 
 def main() -> int:
-    with open("queries.txt") as file:
-        strings = [line.rstrip() for line in file]
+    pid = os.fork()
 
-    clean_strings = []
-    for line in strings:
-        if line.strip():
-            clean_strings.append(line)
-    
-    try:
-        with open("position.txt") as file:
-            i = int(file.read().strip())
-    except:
-        i = 1
-    
-    while True:
-        print(clean_strings[i % len(clean_strings)])
-        i += 1
-        with open("position.txt", "w") as file:
-            file.write(str(i))
-        time.sleep(1)
-    return 0
+    if pid:
+        stoped = False
+        cont = False
+        while True:
+            i = input()
+            if   i == 'k':
+                os.kill(pid, signal.SIGKILL)
+                print("exiting")
+                sys.exit(0)
+            elif i == 's':
+                if not stoped:
+                    os.kill(pid, signal.SIGSTOP)
+                    stoped = True
+                    cont = False
+                    print("stoped")
+            elif i == 'c':
+                if not cont:
+                    os.kill(pid, signal.SIGCONT)
+                    stoped = False
+                    cont = True
+                    print("continue")
+
+
+    else:
+        with open("queries.txt") as file:
+            strings = [line.rstrip() for line in file]
+
+        clean_strings = []
+        for line in strings:
+            if line.strip():
+                clean_strings.append(line)
+        
+        try:
+            with open("position.txt") as file:
+                i = int(file.read().strip())
+        except:
+            i = 1
+        
+        while True:
+            print(clean_strings[i % len(clean_strings)])
+            i += 1
+            with open("position.txt", "w") as file:
+                file.write(str(i))
+            time.sleep(1)
 
 if __name__ == "__main__":
     sys.exit(main())

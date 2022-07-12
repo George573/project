23b5cba2198bea3057068d02534e69a3dbdc056a
George573 <charlotteuwu@192.168.1.75>
2022-07-12 03:29:42 +0300

    31th commit

--- search.py
+++ search.py
@@ -1,4 +1,4 @@
-import time, sys, requests, bs4, re
+import time, sys, requests, bs4, re, settings
 
 class search:
     def __init__(self, queries_file, position = "", start_over = False, links_file = "search.txt", position_file = "position.txt") -> None:
